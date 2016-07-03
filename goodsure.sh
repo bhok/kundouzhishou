574d0ab06e0aac31a2c3e315e320960a6b73c880
kundouzhishou <kundouzhishou@gmail.com>
2016-07-03 15:32:50 +0800

    init directories

--- goodsure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git_dir=/Users/jayson/Documents/git
-my_dir=$git_dir/kundouzhishou
-gs_dir=$git_dir/goodsure_python
-rm -rf $my_dir/goodsure.log
-cd $gs_dir
-python goodsure.py -w 40 -m 15000 -s > $my_dir/goodsure.log
